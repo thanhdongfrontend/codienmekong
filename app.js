0419580f65e49e156ebb60fbafa9e8ff5a391420
thanhdongfrontend <thanhdongfrontend@gmail.com>
2022-10-09 17:14:34 +0700

    first commit

--- app.js
+++ app.js
@@ -90,3 +90,17 @@ forEach(link =>{
     link.classList.add('active');
   }
 })
+
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert("Đã gửi tin nhắn thành công")
+  });
+});
